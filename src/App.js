bac445aba63abbacd7f3e68861f2bfee614d2bc8
xhoana <xhoi.qesja@gmail.com>
2021-09-15 22:54:04 +0200

    react routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
-import './App.css';
-import HomePage from './pages/homepage/homepage.component';
+import "./App.css";
+// import HomePage from "./pages/homepage/homepage.component";
+import { Route } from "react-router";
+
+const HomePage = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <h1>HomePage</h1>
+    </div>
+  );
+};
+const TopicsList = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <h1>TopicsList</h1>
+    </div>
+  );
+};
+const TopicDetail = (props) => {
+  console.log(props)
+  return (
+    <div>
+     <h1>{props.match.params.topicId}</h1>
+    </div>
+  );
+};
 
 function App() {
   return (
     <div>
-     <HomePage/>
+      <Route exact path="/" component={HomePage} />
+      <Route exact path="/topics" component={TopicsList} />
+      <Route exact path="/topics/:topicId" component={TopicDetail} />
     </div>
   );
 }
