487a28f786e47b373510ce19c8364a7f2e847ca1
xhoana <xhoi.qesja@gmail.com>
2021-09-17 16:43:52 +0200

    storing user data in our app

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 
-import { auth } from "./firebase/firebase.utils";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
 class App extends React.Component {
   state = {
@@ -20,10 +20,20 @@ class App extends React.Component {
 
   componentDidMount() {
     // onAuthStateChanged  firebase method that creates a stream whenever the authentication changes (a subscription is created)
-    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
-      this.setState({ currentUser: user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+        userRef.onSnapshot((snapShot) => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data(),
+            },
+          },()=>{  console.log("current user state", this.state);});
+        });
+      }
 
-      //console.log(user);
+      this.setState({ currentUser: userAuth });
     });
   }
 
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,6 +1,6 @@
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/auth';// for authentication
-import 'firebase/compat/firestore';// for db
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth"; // for authentication
+import "firebase/compat/firestore"; // for db
 
 const config = {
   apiKey: "AIzaSyDdSrFqe8Co2eGrgNf63T-Mq0Rg0i2SgCY",
@@ -12,15 +12,53 @@ const config = {
   measurementId: "G-6QP6MKK5E7",
 };
 
+// getting the user auth object that we qet from the authentication library, and store it in the db
+// this function will be async because we are making an API request
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  // returns the correct data from the authentication
+  // it has properties like exist that tells if the user exists in the db, the id etc
+  const snapShot = await userRef.get();
+
+  ////////////////////////////////////////////////////////////////
+  /////////////// if the user doesnt exists we create it/////////
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData,
+      });
+    } catch (error) {
+      console.log("error creating user", error.message);
+    }
+  }
+
+  //we will always return userRef because there is a chance that we can use it for other things
+  return userRef;
+};
+////////////////////////////////////////////////////////////////
+
 firebase.initializeApp(config);
 
+//////////////////////////////////////////////////////////////
+///////////////////SIGN IN////////////////////////////////////
+//////////////////////////////////////////////////////////////
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
-const provider= new firebase.auth.GoogleAuthProvider()
+const provider = new firebase.auth.GoogleAuthProvider();
 
-provider.setCustomParameters({prompt:'select_account'}); 
+provider.setCustomParameters({ prompt: "select_account" });
 
-export const signInWithGoogle=()=> auth.signInWithPopup(provider)
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+/////////////////////////////////////////////////////////////////
 
 export default firebase;
