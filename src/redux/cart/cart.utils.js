e59c8673509666eb220b9f269238d156b66a9260
xhoana <xhoi.qesja@gmail.com>
2021-09-21 10:38:44 +0200

    updated shop data

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,5 +1,24 @@
 /**
  * utility functions allow us to keep our files clean and organize functions that
  * we may need in multiple files in one location
- *  */  
+ */
 
+export const addItemToCart = (cartItems, cartItemToAdd) => {
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === cartItemToAdd.id
+  );
+
+  if (existingCartItem) {
+    return cartItems.map((cartItem) =>
+      cartItem.id === cartItemToAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem
+    );
+  }
+
+  return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
+  /** quantity property gets attached
+   * the first time around since this if block wont't run when its a new item
+   */
+};
+/**this will be imported in the cart reducer */
